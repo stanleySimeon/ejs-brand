baa3fed835faebdb64e35430d02edd045d424118
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-01-25 22:02:42 -0500

    Project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-black text-ejs">Hello CodeSandbox</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        nayanika: ['Nayanika', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        ejs: {
+          DEFAULT: '#05396d',
+          primary: '#6355ac',
+          secondary: '#00b3d1',
+          gray: '#a9b1b7',
+          white: '#ffffff',
+        },
+      },
+      container: {
+        center: true,
+        width: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
+        },
+      },
+    },
+  },
+  /* eslint-disable global-require */
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
+};
